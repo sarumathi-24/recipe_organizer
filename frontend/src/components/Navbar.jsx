5b0035cddb059d1c50ff7c38f66fa94316a3deda
sarumathi <Sarumathin24@gmail.com>
2026-08-10 14:03:44 +0530

    add new step

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
   const { user, logout } = useAuth();
+  const location = useLocation();
   const navigate = useNavigate();
+  const isHomePage = location.pathname === "/";
 
   function handleLogout() {
     logout();
@@ -14,7 +16,7 @@ export default function Navbar() {
     <header className="nav">
       <Link to="/" className="brand">DishBook</Link>
       <nav className="nav-links">
-        <NavLink to="/" end>Home</NavLink>
+        {!isHomePage && <NavLink to="/" end>Home</NavLink>}
         {user && <NavLink to="/recipes" end>Recipes</NavLink>}
         {user && <NavLink to="/recipes/new">Add Dish</NavLink>}
         {user && <NavLink to="/my-recipes">My Recipes</NavLink>}
